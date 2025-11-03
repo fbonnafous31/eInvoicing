731fc592aec545812dc30e4ecf0a3a086a67b19f
François <francois.bonnafous@hotmail.fr>
2025-11-03 17:48:39 +0100

    fix: ajout SSL pour connexion PostgreSQL sur Render

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -14,6 +14,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT),
+  ssl: {
+    rejectUnauthorized: false // Render 
+  },
 });
 
 module.exports = pool;
