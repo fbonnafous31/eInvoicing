60f86b26c439939c637cba84feb21667269dbdbb
François <francois.bonnafous@hotmail.fr>
2026-04-01 11:24:24 +0200

    Mise à jour 012_add_original_quote_to_invoices.sql

--- backend/sql/migrations/012_add_original_quote_to_invoices.sql
+++ backend/sql/migrations/012_add_original_quote_to_invoices.sql
@@ -1,4 +1,4 @@
 -- migrations/012_add_original_quote_to_invoices.sql
 
 ALTER TABLE invoicing.invoices
-ADD COLUMN original_quote_number VARCHAR(50);
+ADD COLUMN IF NOT EXISTS original_quote_number VARCHAR(50);
